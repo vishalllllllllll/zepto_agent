55cda9812d407a9c5c9697c4bf53c37c85d40f93
Vishal M <135323838+vishalllllllllll@users.noreply.github.com>
2025-08-16 01:06:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 
 options = webdriver.ChromeOptions()
-options.add_argument(r"--user-data-dir=C:\Users\Visha\vscode project files\zepto_agent\chrome_profile")
+options.add_argument(r"your-chrome-profile-path")
 options.add_experimental_option("detach", True)  # <-- keeps Chrome open
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.zeptonow.com/")
+
